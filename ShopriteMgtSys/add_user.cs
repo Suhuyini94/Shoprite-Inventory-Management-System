904df8f17c0922e373b3de3ee5035a3f4ba3c17f
ONEHEART <ONEHEART@SUHUYINI>
2022-08-31 09:00:25 +0000

    Add project files.

---
+++ ShopriteMgtSys/add_user.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ShopriteMgtSys
+{
+    public partial class add_user : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\SHA'BAN\\source\\repos\\ShopriteMgtSys\\ShopriteMgtSys\\inventory.mdf\";Integrated Security=True");
+
+        public add_user()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            SqlCommand scmd = conn.CreateCommand();
+            scmd.CommandType = CommandType.Text;
+            scmd.CommandText = "select * from registration where username='" + textBox3.Text + "'";
+            scmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(scmd);
+            sda.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (i == 0)
+            {
+                SqlCommand scmd1 = conn.CreateCommand();
+                scmd1.CommandType = CommandType.Text;
+                scmd1.CommandText = "insert into registration values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "')";
+
+                textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = "";
+                textBox4.Text = ""; textBox5.Text = ""; textBox6.Text = "";
+                display();
+
+                scmd1.ExecuteNonQuery();
+
+                MessageBox.Show("User added successfully");
+
+
+            }
+            else
+            {
+                MessageBox.Show("This username is taken. Please choose another one!");
+            }
+        }
+
+        private void add_user_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn.Open();
+            display();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void display()
+        {
+            SqlCommand scmd = conn.CreateCommand();
+            scmd.CommandType = CommandType.Text;
+            scmd.CommandText = "select * from registration";
+            scmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(scmd);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id;
+            id = Convert.ToInt32(dataGridView1.SelectedCells[0].Value.ToString());
+            SqlCommand scmd = conn.CreateCommand();
+            scmd.CommandType = CommandType.Text;
+            scmd.CommandText = "delete from registration where id= "+ id +"";
+            scmd.ExecuteNonQuery();
+
+            display();
+        }
+    }
+}
